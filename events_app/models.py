e5e45bf24b27b0aa0129f299348d2907f83b14e1
Ceina Ellison <ceina.ellison@gmail.com>
2025-04-22 20:26:09 -0400

    edited model classes

--- events_app/models.py
+++ events_app/models.py
@@ -1,32 +1,33 @@
 """Create database models to represent tables."""
 from events_app import db
 from sqlalchemy.orm import backref
+from sqlalchemy import Enum
+import enum
 
-# TODO: Create a model called `Guest` with the following fields:
-# - id: primary key
-# - name: String column
-# - email: String column
-# - phone: String column
-# - events_attending: relationship to "Event" table with a secondary table
+# Many-to-Many Association Table
+guest_event_table = db.Table(
+    'guest_event',
+    db.Column('event_id', db.Integer, db.ForeignKey('event_id'), primary_key=True),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest_id'), primary_key=True)
+)
+
+class EventType(enum.Enum):
+    Party = 'Party'
+    Study = 'Study'
+    Networking = 'Networking'
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-
-# TODO: Create a model called `Event` with the following fields:
-# - id: primary key
-# - title: String column
-# - description: String column
-# - date_and_time: DateTime column
-# - guests: relationship to "Guest" table with a secondary table
-
-# STRETCH CHALLENGE: Add a field `event_type` as an Enum column that denotes the
-# type of event (Party, Study, Networking, etc)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    phone = db.Column(db.String(80))
+    events_attending = db.relationship('Event', secondary=guest_event_table,back_populates='guests')
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(100), nullable=False)
+    date_and_time = db.Column(db.DateTime)
+    event_type = db.Column(Enum(EventType))
+    guests = db.relationship('Guest', secondary=guest_event_table, back_populates='events_attending')
 
-# TODO: Create a table `guest_event_table` with the following columns:
-# - event_id: Integer column (foreign key)
-# - guest_id: Integer column (foreign key)
-
-guest_event_table = None
